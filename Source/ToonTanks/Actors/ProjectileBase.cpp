9ca621c4c9bf15cbceb073b6ad5721f9cfb1f38d
Arjun Chauhan <arjunchauhan007@gmail.com>
2021-03-02 21:19:05 +0530

    Enable bullet collision through delegate

--- Source/ToonTanks/Actors/ProjectileBase.cpp
+++ Source/ToonTanks/Actors/ProjectileBase.cpp
@@ -3,6 +3,7 @@
 
 #include "ProjectileBase.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 // Sets default values
 AProjectileBase::AProjectileBase()
@@ -23,5 +24,23 @@ AProjectileBase::AProjectileBase()
 void AProjectileBase::BeginPlay()
 {
 	Super::BeginPlay();
+
+	ProjectileMesh->OnComponentHit.AddDynamic(this, &AProjectileBase::OnHit);
 }
 
+void AProjectileBase::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	AActor* MyOwner = GetOwner();
+
+	if (!MyOwner)
+	{
+		return;
+	}
+
+	if (OtherActor && OtherActor != this && OtherActor != MyOwner)
+	{
+		UGameplayStatics::ApplyDamage(OtherActor, Damage, MyOwner->GetInstigatorController(), this, DamageType);
+	}
+
+	Destroy();
+}
--- Source/ToonTanks/Pawns/PawnTurret.cpp
+++ Source/ToonTanks/Pawns/PawnTurret.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "PawnTank.h"
 #include "PawnTurret.h"
+#include "PawnTank.h"
 #include "Kismet/GameplayStatics.h"
 
 // Called when the game starts or when spawned
